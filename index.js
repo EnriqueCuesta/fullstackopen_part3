5b089ea1f3f9bb6fa33221ef1944dca142b1afe2
Enrique Cuesta <ecuesta@gmail.com>
2021-08-05 20:56:46 +0200

    Second commit

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people </p><p>${Date()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
